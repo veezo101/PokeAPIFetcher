30a73faa5de76c3a36772ccfe46cddb85b4c15d8
veezo101 <aviral109@hotmail.com>
2023-11-07 21:23:30 +0400

    super inefficient ver that dies with forms

--- tmpuller.py
+++ tmpuller.py
@@ -8,16 +8,26 @@ base_url = "https://pokeapi.co/api/v2/"
 response = requests.get(f"{base_url}move/{move_number}/")
 move_data = response.json()
 
-# Extract the moves that the TM teaches
 lst_pokemonThatLearn = move_data['learned_by_pokemon']
 
-# Iterate through the moves and get the Pokémon that can learn each move
-pokemon_list = []
+pokemon_list_names = []
+pokemon_list_numbers = []
+
 for selectedPokemon in lst_pokemonThatLearn:
-    selectedPokemonResponse = requests.get(selectedPokemon['url'])
-    lstMoves = selectedPokemonResponse.json()['moves']
+    selectedPokemonResponse = requests.get(selectedPokemon['url']).json()
+    lstMoves = selectedPokemonResponse['moves']
     for moveObj in lstMoves:
-        for versionInfo in moveObj['version_group_details']:
-            if versionInfo['version_group']['name']=='ultra-sun-ultra-moon':
-                if versionInfo['move_learn_method']['name']=='machine':
-                    print(f"{selectedPokemon['name']} - {moveObj['move']['name']}")
+        if moveObj['move']['name'] == move_data['name']:
+            for versionInfo in moveObj['version_group_details']:
+                if versionInfo['version_group']['name']=='ultra-sun-ultra-moon':
+                    if versionInfo['move_learn_method']['name']=='machine':
+                        pokemon_list_names.append(selectedPokemon['name'])
+                        speciesResponse = requests.get(selectedPokemonResponse['species']['url'])
+                        species_data = speciesResponse.json()
+                        pokemon_list_numbers.append(species_data['id'])
+                        print(pokemon_list_names)
+                        print(pokemon_list_numbers)
+                        break
+            break
+print(pokemon_list_names)
+print(pokemon_list_numbers)
